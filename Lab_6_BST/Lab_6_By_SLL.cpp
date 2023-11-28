275ce2d17656a3ae55ffe023233b7806734b5641
M. Nura Alam Naim <nuraalomnaim@gmail.com>
2023-11-29 03:39:02 +0600

    WIn

---
+++ Lab_6_BST/Lab_6_By_SLL.cpp
@@ -0,0 +1,181 @@
+//|ADMIRAL_AUDITORE|//
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define nl "\n"
+#define null NULL
+struct node
+{
+    ll value;
+    struct node *left = null, *right = null;
+} *root = null, *temp = null;
+struct node *insert(struct node *root, ll val)
+{
+    if (!root)
+    {
+        root = (struct node *)malloc(sizeof(struct node));
+        root->value = val;
+        return root;
+    }
+    else if (val < root->value)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+void inorder(struct node *root)
+{
+    if (!root)
+        return;
+    inorder(root->left);
+    cout << root->value << ' ';
+    inorder(root->right);
+}
+void postorder(struct node *root)
+{
+    if (!root)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->value << ' ';
+}
+void preorder(struct node *root)
+{
+    if (!root)
+        return;
+    cout << root->value << ' ';
+    preorder(root->left);
+    preorder(root->right);
+}
+struct node *search(struct node *root, ll val)
+{
+    if (!root || root->value == val)
+        return root;
+    else if (root->value > val)
+        return search(root->left, val);
+    else
+        return search(root->right, val);
+}
+struct node *searchMax(struct node *root)
+{
+    if (!root->right)
+    {
+        cout << root->value << nl;
+        return root;
+    }
+    else
+        searchMax(root->right);
+}
+struct node *searchMin(struct node *root)
+{
+    if (!root->left)
+    {
+        cout << root->value << nl;
+        return root;
+    }
+    else
+        searchMin(root->left);
+}
+struct node *Delete(struct node *root, ll val)
+{
+    if (!root)
+        return root;
+    if (root->value > val)
+        root->left = Delete(root->left, val);
+    if (root->value < val)
+        root->right = Delete(root->right, val);
+    else
+    {
+        if (!root->left && !root->right)
+        {
+            free(root);
+            root = null;
+            return root;
+        }
+        else if (!root->right)
+        {
+            temp = root;
+            root = root->left;
+            free(temp);
+            return root;
+        }
+        else if (!root->left)
+        {
+            temp = root;
+            root = root->right;
+            free(temp);
+            return root;
+        }
+        else
+        {
+            temp = searchMin(root->right);
+            root->value = temp->value;
+            root->right = Delete(root->right, temp->value);
+            return root;
+        }
+    }
+}
+int main()
+{
+    ll choice, num;
+    while (true)
+    {
+        cout << nl<<nl;
+        cout << 1 << ".Insert to BST." << nl;
+        cout << 2 << ".Print Preorder of BST." << nl;
+        cout << 3 << ".Print Inorder of BST." << nl;
+        cout << 4 << ".Print Postorder of BST." << nl;
+        cout << 5 << ".Search element in BST." << nl;
+        cout << 6 << ".Print Mininum value of BST." << nl;
+        cout << 7 << ".Print Maximum value of BST." << nl;
+        cout << 8 << ".Delete value from BST" << nl;
+        cout << "#Enter Choice: ";
+        cin >> choice;
+        if (choice == 1)
+        {
+            cout << "Enter value to insert: ";
+            cin >> num;
+            if (!root)
+                root = insert(root, num);
+            else
+                insert(root, num);
+        }
+        else if (choice == 2)
+        {
+            preorder(root);
+        }
+        else if (choice == 3)
+        {
+            inorder(root);
+        }
+        else if (choice == 4)
+        {
+            postorder(root);
+        }
+        else if (choice == 5)
+        {
+            cout << "Enter value to search: ";
+            cin >> num;
+            if (search(root, num))
+                cout << "Number is present" << nl;
+            else
+                cout << "Number is not present" << nl;
+        }
+        else if (choice == 6)
+        {
+            searchMin(root);
+        }
+        else if (choice == 7)
+        {
+            searchMax(root);
+        }
+        else if (choice == 8)
+        {
+            cout << "Enter value to Delete: ";
+            cin >> num;
+            if(!root)
+                cout << "Tree is empty" << nl;
+            Delete(root, num);
+        }
+    }
+}
