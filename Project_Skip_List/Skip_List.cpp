b06052f7bc44ab234acaeac73f40678fd4872dde
M. Nura Alam Naim <nuraalomnaim@gmail.com>
2023-12-25 15:12:22 +0600

    25/12/23

---
+++ Project_Skip_List/Skip_List.cpp
@@ -0,0 +1,37 @@
+//|ADMIRAL_AUDITORE|//
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define nl "\n"
+#define null NULL
+struct node
+{
+    int data;
+    struct node *next = null;
+    struct node *previous = null;
+    struct node *up = null;
+    struct node *down = null;
+};
+struct node *head = (struct node *)malloc(sizeof(struct node));
+struct node *tail = (struct node *)malloc(sizeof(struct node));
+struct node* search(int val)
+{
+    struct node *temp = head;
+    while(temp->down!=null)
+    {
+        while(val>=temp->down->data)
+        {
+            temp = temp->next;
+        }
+    }
+    return temp;
+}
+
+int level = 0;
+int main()
+{
+    head->data = INT_MIN;
+    tail->data = INT_MAX;
+    head->next = tail;
+    tail->previous = head;
+}
